5d63a8582e2fd6333eb841e1e5114afd75f49c91
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2015-11-12 21:16:37 -0500

    Initial boilerplate

---
+++ webpack.defaults.js
@@ -0,0 +1,22 @@
+var path = require('path')
+var autoprefixer = require('autoprefixer')
+var cssnext = require('cssnext')
+
+module.exports = {
+  entry: [
+    './src/client/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', include: path.join(__dirname, 'src') },
+      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
+    ]
+  },
+  postcss: [cssnext, autoprefixer]
+}
